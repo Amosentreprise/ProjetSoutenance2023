8553e5c337d8e5ac393b3dadaa2a6fc500c42141
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2023-04-14 11:26:16 +0200

    Annimation d'acceuil

--- frontendGestionLapin/vue-frontendGestionLapin/src/components/Card.vue
+++ frontendGestionLapin/vue-frontendGestionLapin/src/components/Card.vue
@@ -3,7 +3,7 @@
 
     <div class="leading-normal">
      <h1 class="mb-1 text-2xl font-bold text-other pt-10">{{ fonctionnalite }}</h1>
-     <p class="mb-2 text-other pt-6">{{ description }}</p>
+     <p class="mb-2 text-other pt-6 w-96">{{ description }}</p>
     </div>
     <div>
       <img :src="src" :alt="description" class="w-auto h-[250px] object-cover rounded-md">
--- frontendGestionLapin/vue-frontendGestionLapin/src/views/commons/Welcome.vue
+++ frontendGestionLapin/vue-frontendGestionLapin/src/views/commons/Welcome.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="h-screen w-full flex flex-col justify-center">
-        <div>
+        <div v-for="card in cards" :key="card.id">
        <Card 
-        :description="cards[2].description"
-        :fonctionnalite="cards[2].fonctionnalite"
-        :src="cards[2].image"
+        :description="card.description"
+        :fonctionnalite="card.fonctionnalite"
+        :src="card.image"
+        v-if="card.id === index"
         
        />
      
@@ -23,8 +24,11 @@
 import { Carousel } from 'flowbite'
 import Card from "../../components/Card.vue"
 import Button from "../../components/Button.vue"
-import image1 from "../../assets/Welcome/image1.jpeg";
-import image2 from "../../assets/Welcome/image2.jpeg";
+import codeqr from "../../assets/Welcome/codeqr.jpg";
+import hommeAfaire from "../../assets/Welcome/hommeAfaire.jpg";
+import lapin1 from "../../assets/Welcome/lapin1.jpeg";
+import lapin2 from "../../assets/Welcome/lapin2.jpg";
+import scannage from "../../assets/Welcome/scannage.jpg";
 
 
 
@@ -37,25 +41,55 @@ export default {
     },
     data() {
         return {
-            nameButton:'Demarrer',
+            nameButton: 'Demarrer',
+            index: 0,
             cards: [{
                 id:0,
-                fonctionnalite: 'FONCTIONNALITE 1',
-                description: 'FONCTIONNALITE1',
-                image:image1
+                fonctionnalite: "BIENVENUE SUR L'application",
+                description: "Une application simple qui vous aidera à mieux gerer votre élevage de lapin. Elle a juste besoin de certaine information sur le lapin.",
+                image:lapin2
            },{
                 id:1,
-                fonctionnalite: 'FONCTIONNALITE 2',
-                description: 'kjdcjvdbnjn',
-                image:image2
-           },{
+                fonctionnalite: 'Eleveur ?',
+                description: "Pense à autre chose l'application se charge de tout.",
+                image:hommeAfaire
+                },{
                 id:2,
-                fonctionnalite: 'FONCTIONNALITE 3',
-                description: 'loremkckkkkkkkkkkkkkkk',
-                image:image1
+                fonctionnalite: "Homme d'affaire ?",
+                description: "Crée un compte et ajoute un eleveur pour te gérer la ferme. Tu peux voir les statisques",
+                image:hommeAfaire
+           }
+                , {
+                id:3,
+                fonctionnalite: 'Tu veux eviter les problemes de consanguinités ?',
+                description: "La plateforme predire le cycle de reproduction, te donne des recommandations pour eviter les problemes de consanguité lors de l'accouplement.",
+                image:lapin1
+                }
+             , {
+                id:4,
+                fonctionnalite: 'Enregistrement de lapin',
+                description: "A chaque enregitrement, le système te genère un code qr. Telecharge juste le code qr, Greffe ce code qr au niveau de ton lapin.",
+                image:codeqr
+           }, {
+                id:5,
+                fonctionnalite: 'Tu veux gerer la vente ? le decès ?',
+                description: "Faire juste le scannage de ce code qr pour gerer la vente et le decès ",
+                image:scannage
            }]
         };
     },
+    mounted() {
+        setInterval(this.nextChild, 3000);
+    },
+      methods: {
+    nextChild() {
+      if (this.index === this.cards.length - 1) {
+        this.index = 0;
+      } else {
+        this.index++;
+      }
+    },
+  },
 }
 
 
