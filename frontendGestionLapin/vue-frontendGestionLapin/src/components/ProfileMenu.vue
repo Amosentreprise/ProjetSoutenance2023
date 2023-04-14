3792297c8a0fee30eb62f8e38a8574650018fad9
Amos SOSSOUKPE <fenouamossossoukpe@gmail.com>
2023-04-14 12:33:23 +0200

    creation du composant ProfileMenu

---
+++ frontendGestionLapin/vue-frontendGestionLapin/src/components/ProfileMenu.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-white p-4 w-40 h-100">
+        
+        <div v-for="option in options" :key="option.id" >
+           <router-link :to="option.lien">
+            <div class="flex items-center shadow-sm hover:bg-hoverboard p-2 cursor-pointer">
+            <span>{{ option.icon }}</span>
+            <span>{{ option.optionName }}</span>
+            </div>
+            </router-link>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'profileMenu',
+data() {
+    return {
+        options: [{
+            id:0,
+            icone: '',
+            lien: '',
+            optionName:'Voir mon profil'
+        },{
+            id:1,
+            icone: '',
+            lien: '',
+            optionName:'Ajouter un eleveur'
+        },
+        {
+            id:2,
+            icone: '',
+            lien: '',
+            optionName:'basculer vers une ferme'
+        },{
+            id:3,
+            icone: '',
+            lien: '/Connexion',
+            optionName:'Se deconnecter'
+        }],
+    };
+},
+    
+}
+</script>
--- frontendGestionLapin/vue-frontendGestionLapin/src/views/boardUsers/boardUser.vue
+++ frontendGestionLapin/vue-frontendGestionLapin/src/views/boardUsers/boardUser.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="relative">
 <SideBar :nameDashboard="nameDashboard" :ActionName="ActionName">
     <template v-slot:options>
         <ul class="py-4">
@@ -11,17 +11,18 @@
         </li>
       </ul>
     </template>
-    <template v-slot:other>
-        <button class="bg-gray-200 rounded-full p-2 mr-2">
+    <template v-slot:other >
+        <span class="bg-gray-200 rounded-full p-2 mr-2">
             <svg class="h-6 w-6 fill-current text-white" viewBox="0 0 24 24">
               <path d="M12 21c1.1 0 2-.9 2-2H10c0 1.1.9 2 2 2zm6-6V8c0-3.9-2.5-7.2-6-8.5V0h-4v.5C8.5.8 6 4.1 6 8v7l-3.3 3.3c-.5.5-.7 1.2-.5 1.9L4 22h16l.8-4.8c.2-.7 0-1.4-.5-1.9L18 15z" />
             </svg>
-          </button>
-          <button class="bg-gray-200 rounded-full p-2">
+        </span>
+          <span class="bg-gray-200 rounded-full p-2" @mouseover="showMenu = true ">
             <svg class="h-6 w-6 fill-current text-white" viewBox="0 0 24 24">
               <path d="M12 12a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm7.26 6.63l-1.68-1.26a1 1 0 0 0-1.18 0l-1.67 1.26a8 8 0 1 0 3.53 0z" />
             </svg>
-          </button>
+        </span>
+          <ProfileMenu v-if="showMenu" class="absolute top-20 right-3" @mouseleave="showMenu = false"/>
     </template>
     <template v-slot:right>
         <router-view/>
@@ -31,17 +32,20 @@
 </template>
 <script>
 import SideBar from "../../components/SideBar.vue"
+import ProfileMenu from "../../components/ProfileMenu.vue";
 
 export default {
 
     components: {
         SideBar,
+        ProfileMenu
        
     },
     data() {
         return {
             nameDashboard: 'Ferme A',
             ActionName: 'Dashboard',
+            showMenu:false,
 
             options: [
                 {
@@ -93,7 +97,12 @@ export default {
             ]
             
         };
+       
     },
+    methods: {
+      
+            
+        }
 }
 
 </script>
